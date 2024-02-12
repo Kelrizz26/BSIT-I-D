57660e01bf5a7467a2a1ed843124fb342d8377ac
Kelrizz26 <158041133+Kelrizz26@users.noreply.github.com>
2024-02-12 18:18:34 +0800

    Update CAMPOS_ACTIVITY_V.java

--- Activities/Activity V/CAMPOS_ACTIVITY_V.java
+++ Activities/Activity V/CAMPOS_ACTIVITY_V.java
@@ -4,13 +4,13 @@ public static void main(String []args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your age: ");
     int age = sc.nextInt();
-    if (age<=11){
+    if (age==0 && age<=11){
         System.out.println("Child");
      }
-     if else (age>=12 && age<=17){
+     if else (age==12 && age<=17){
         System.out.println("Teen");
-     }
-     if else (age>=18 && age<=64){
+     
+     } else (age==18 && age<=64){
         System.out.println("Adult");
      }
 }
